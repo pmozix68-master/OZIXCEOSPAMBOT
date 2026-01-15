81f9a92a58ceb0ec937fddf147a12e87da4bb110
ozixceo <pmozix0786@gmail.com>
2026-01-15 17:14:28 +0530

    Update bot.py

--- HeartHacker/plugins/bot.py
+++ HeartHacker/plugins/bot.py
@@ -15,7 +15,7 @@ from datetime import datetime
 RIZ_PIC = ALIVE_PIC if ALIVE_PIC else "https://telegra.ph/file/ba87c58f01a6fcb5ef512.jpg"
   
 
-rizoel = "✯ 𝙃𝙚𝙖𝙧𝙩𝙃𝙖𝙘𝙠𝙚𝙧 𝙎𝙥𝙖𝙢 𝙃𝙚𝙧𝙚 ✯\n\n"
+rizoel = "✯ OZIX SPAM HERE ✯\n\n"
 rizoel += f"═══════════════════\n"
 rizoel += f"• **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ** : `3.9.6`\n"
 rizoel += f"• **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ** : `{version.__version__}`\n"
@@ -30,11 +30,11 @@ async def alive(event):
                                   caption=rizoel,
                                   buttons=[
         [
-        Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/YamlokOfficial"),
-        Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/LHTHALxFIGHTERS")
+        Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/+KKYgpQNCwbgwODdl"),
+        Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/OZIXSUPPORT")
         ],
         [
-        Button.url("• ʀᴇᴘᴏ •", "https://github.com/AT-WORLDS-END/HeartHackerSpamBot")
+        Button.url("• ʀᴇᴘᴏ •", "https://github.com/pmozix68-master")
         ]
         ]
         )
@@ -83,7 +83,7 @@ async def ping(e):
         event = await e.reply(text, parse_mode=None, link_preview=None )
         end = datetime.now()
         ms = (end-start).microseconds / 1000
-        await event.edit(f"𝐏𝐎𝐍𝐆\n\nϟ 𝙃𝙚𝙖𝙧𝙩 𝙃𝙖𝙘𝙠𝙚𝙧 𝙎𝙥𝙖𝙢 𝘽𝙤𝙩 ϟ︎ `{ms}` ᴍs")
+        await event.edit(f"𝐏𝐎𝐍𝐆\n\nϟ 𝙊𝙕𝙄𝙓 𝘾𝙀𝙊 𝙎𝙥𝙖𝙢 𝘽𝙤𝙩 ϟ︎ `{ms}` ᴍs")
         
         
 
@@ -99,7 +99,7 @@ async def ping(e):
 @Riz10.on(events.NewMessage(incoming=True, pattern=r"\%srestart(?: |$)(.*)" % hl))
 async def restart(e):
     if e.sender_id in SUDO_USERS:
-        text = "**Restarting Your 𝙃𝙚𝙖𝙧𝙩 𝙃𝙖𝙘𝙠𝙚𝙧 𝙎𝙥𝙖𝙢 𝘽𝙤𝙩**.. Please Wait Until It Starts Again"
+        text = "**Restarting Your 𝙊𝙕𝙄𝙓 𝘾𝙀𝙊 𝙎𝙥𝙖𝙢 𝘽𝙤𝙩**.. Please Wait Until It Starts Again"
         await e.reply(text, parse_mode=None, link_preview=None)
         try:
             await Riz.disconnect()
@@ -171,7 +171,7 @@ async def tb(event):
             newsudo = f"{sudousers} {target}"
         else:
             newsudo = f"{target}"
-        await ok.edit(f"**Added `{target}` ** as a sudo user 🔱 Restarting.. Please wait a minute...")
+        await ok.edit(f"**Added `{target}` ** as a sudo user 💀 Restarting.. Please wait a minute...")
         heroku_var[rizoel] = newsudo   
    
      
